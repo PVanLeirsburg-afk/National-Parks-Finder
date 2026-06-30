1424aaafaf87ad7e81e0a95c3d2f6b252bfb945d
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-30 17:58:22 -0500

    ChatGPT App

---
+++ National Parks Finder/ParkDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  ParkDetailView.swift
+//  National Parks Finder
+//
+//  Created by Pamela VanLeirsburg on 6/30/26.
+//
+
+import Foundation
---
+++ Park.swift
@@ -0,0 +1,8 @@
+//
+//  Park.swift
+//  National Parks Finder
+//
+//  Created by Pamela VanLeirsburg on 6/30/26.
+//
+
+import Foundation
